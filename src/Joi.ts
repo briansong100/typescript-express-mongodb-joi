271394c55f06f0885035ee9b3a33569752546ec9
Brian Okhwon Song <91863706+briansong100@users.noreply.github.com>
2022-05-14 01:20:36 +0800

    Update Joi.ts

--- src/Joi.ts
+++ src/Joi.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-const schema = Joi.object({
+const schema:Joi.ObjectSchema  = Joi.object({
   name: Joi.string().required(), //string().min().max.alphnum()
   age: Joi.number().integer().min(1).max(80).required(),
   amount: Joi.number(),
@@ -31,16 +31,12 @@ if (error) {
   console.log({ value }); //res.status(400).json({"error": error.details[0]})
 }
 
-export class Contact {
-  name: string;
-  preferred_method: string;
-  email: string;
-  telephone: string;
 
-  static validationSchema: Joi.ObjectSchema = Joi.object({
+// optional validate
+
+ const optionalSchema : Joi.ObjectSchema = Joi.object({
     name: Joi.string().required(),
     preferred_method: Joi.string().required().valid('Email', 'Telephone'),
     email: Joi.string().email().when('preferred_method', { is: 'Email', then: Joi.required() }),
     telephone: Joi.string().when('preferred_method', { is: 'Telephone', then: Joi.required() })
   });
-}
